7c004600fe95f83983b20493bc3e4057732b41f9
Season Travel <system.seasonthailand@gmail.com>
2025-04-01 17:12:16 +0700

    Add files via upload

---
+++ create-new-package-important-hotel-info.js
@@ -0,0 +1,17 @@
+
+
+
+const hotelMessageInfoArray = [
+    {
+        hotelName: "Dusit Thani Laguna Phuket",
+        messageInfo_p1: "حجز 3 أيام أو أكثر = فري ابجريد",
+        messageInfo_p2: "من ديلوكس لاجون فيو الى ديلوكس سي فيو",
+        messageInfo_p3: "أو",
+        messageInfo_p4: "من ديلوكس لاجون فيو الى بريمير اوشن فرونت",
+    },
+    {
+        hotelName: "Holiday Inn Express Phuket",
+        messageInfo_p1: "حجز 3 أيام او أكثر = خصم 100 بات من سعر الليلة",
+        messageInfo_p2: "(صالح للإستخدام الى 31 مايو)",
+    }
+];
