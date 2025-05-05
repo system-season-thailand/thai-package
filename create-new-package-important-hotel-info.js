180519917e8c57c3ee76f8b38c37786d2b549b3d
Season Travel <system.seasonthailand@gmail.com>
2025-05-05 20:20:46 +0700

    Update create-new-package-important-hotel-info.js

--- create-new-package-important-hotel-info.js
+++ create-new-package-important-hotel-info.js
@@ -1,17 +1,1 @@
-
-
-
-const hotelMessageInfoArray = [
-    {
-        hotelName: "Dusit Thani Laguna Phuket",
-        messageInfo_p1: "حجز 3 أيام أو أكثر = فري ابجريد",
-        messageInfo_p2: "من ديلوكس لاجون فيو الى ديلوكس سي فيو",
-        messageInfo_p3: "أو",
-        messageInfo_p4: "من ديلوكس لاجون فيو الى بريمير اوشن فرونت",
-    },
-    {
-        hotelName: "Holiday Inn Express Phuket",
-        messageInfo_p1: "حجز 3 أيام او أكثر = خصم 100 بات من سعر الليلة",
-        messageInfo_p2: "(صالح للإستخدام الى 31 مايو)",
-    }
-];
+const hotelMessageInfoArray=[{hotelName:"Dusit Thani Laguna Phuket",messageInfo_p1:"حجز 3 أيام أو أكثر = فري ابجريد",messageInfo_p2:"من ديلوكس لاجون فيو الى ديلوكس سي فيو",messageInfo_p3:"أو",messageInfo_p4:"من ديلوكس لاجون فيو الى بريمير اوشن فرونت"},{hotelName:"Holiday Inn Express Phuket",messageInfo_p1:"حجز 3 أيام او أكثر = خصم 100 بات من سعر الليلة",messageInfo_p2:"(صالح للإستخدام الى 31 مايو)"},{hotelName:"The Nature Phuket",messageInfo_p1:"حجز 3 أيام او 4 أيام = خصم 100 بات من سعر الليلة",messageInfo_p2:"حجز 5 أيام او أكثر = خصم 200 بات من سعر الليلة",messageInfo_p3:"(صالح للإستخدام الى 31 مايو)"}];
