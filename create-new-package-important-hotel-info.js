cb98ca91c872fab17fc7008e9c619fe00480e9aa
Season Travel <system.seasonthailand@gmail.com>
2025-09-03 17:19:41 +0700

    Update create-new-package-important-hotel-info.js

--- create-new-package-important-hotel-info.js
+++ create-new-package-important-hotel-info.js
@@ -1,1 +1,61 @@
-const hotelMessageInfoArray=[{hotelName:"Dusit Thani Laguna Phuket",messageInfo_p1:"حجز 3 ليالي أو أكثر = فري ابجريد",messageInfo_p2:"من ديلوكس لاجون فيو الى ديلوكس سي فيو",messageInfo_p3:"أو",messageInfo_p4:"من ديلوكس لاجون فيو الى بريمير اوشن فرونت"},{hotelName:"Holiday Inn Express Phuket",messageInfo_p1:"حجز 3 ليالي او أكثر = خصم 100 بات من سعر الليلة",messageInfo_p2:"(صالح للإستخدام الى 31 مايو)"},{hotelName:"The Nature Phuket",messageInfo_p1:"حجز 3 ليالي او 4 ليالي = خصم 100 بات من سعر الليلة",messageInfo_p2:"حجز 5 ليالي او أكثر = خصم 200 بات من سعر الليلة",messageInfo_p3:"(صالح للإستخدام الى 31 مايو)"},{hotelName:"Bandara Pool Villas Phuket",messageInfo_p1:"بندارا فيلا الشخص الي عمره 12 سنة وفوق يعتبر بالغ",messageInfo_p2:"مميزات حجز ليلتين او اكثر",messageInfo_p3:"تزيين على السرير",messageInfo_p4:"افطار عائم لمره واحدة",messageInfo_p5:"سلة حلويات خفيفة عند الاستقبال",messageInfo_p6:"بار صغير مجاني بس شكله حرام"},{hotelName:"Andara Resort Villas Phuket",messageInfo_p1:"حجز 3 ليالي او أكثر = عشاء عائلي مشجاني في مطعم سيلك",messageInfo_p2:"صالح للإستخدام الى 31 اكتوبر 2025 فقط"},{hotelName:"Lancaster Bangkok",messageInfo_p1:"لانكاستر الشخص الي عمره اكثر من 12 سنة يعتبر بالغ"},{hotelName:"Siam Kempinski Bangkok",messageInfo_p1:"سيام كمبينسكي الشخص الي عمره 13 سنة وفوق لازم تأخذ له اكسترا بيد"},{hotelName:"The Charm Resort Phuket",messageInfo_p1:"ذا تشارم الشخص الي عمره 12 سنة وفوق يعتبر بالغ"},{hotelName:"Amari Phuket",messageInfo_p1:"اماري بوكيت الشخص الي عمره 12 سنة وفوق يعتبر بالغ"},{hotelName:"Sofitel Bangkok Sukhumvit",messageInfo_p1:"سوفيتيل بانكوك الشخص الي عمره 12 سنة وفوق يعتبر بالغ"}];
+
+
+
+const hotelMessageInfoArray = [
+    {
+        hotelName: "Dusit Thani Laguna Phuket",
+        messageInfo_p1: "حجز 3 ليالي أو أكثر = فري ابجريد",
+        messageInfo_p2: "من ديلوكس لاجون فيو الى ديلوكس سي فيو",
+        messageInfo_p3: "أو",
+        messageInfo_p4: "من ديلوكس لاجون فيو الى بريمير اوشن فرونت",
+    },
+    {
+        hotelName: "Holiday Inn Express Phuket",
+        messageInfo_p1: "حجز 3 ليالي او أكثر = خصم 100 بات من سعر الليلة",
+        messageInfo_p2: "(صالح للإستخدام الى 31 مايو)",
+    },
+    {
+        hotelName: "The Nature Phuket",
+        messageInfo_p1: "حجز 3 ليالي او 4 ليالي = خصم 100 بات من سعر الليلة",
+        messageInfo_p2: "حجز 5 ليالي او أكثر = خصم 200 بات من سعر الليلة",
+        messageInfo_p3: "(صالح للإستخدام الى 31 مايو)",
+    },
+    {
+        hotelName: "Bandara Pool Villas Phuket",
+        messageInfo_p1: "بندارا فيلا الشخص الي عمره 12 سنة وفوق يعتبر بالغ",
+        messageInfo_p2: "مميزات حجز ليلتين او اكثر",
+        messageInfo_p3: "تزيين على السرير",
+        messageInfo_p4: "افطار عائم لمره واحدة",
+        messageInfo_p5: "سلة حلويات خفيفة عند الاستقبال",
+        messageInfo_p6: "بار صغير مجاني بس شكله حرام",
+    },
+    {
+        hotelName: "Andara Resort Villas Phuket",
+        messageInfo_p1: "حجز 3 ليالي او أكثر = عشاء عائلي مشجاني في مطعم سيلك",
+        messageInfo_p2: "صالح للإستخدام الى 31 اكتوبر 2025 فقط",
+    },
+    {
+        hotelName: "Lancaster Bangkok",
+        messageInfo_p1: "لانكاستر الشخص الي عمره اكثر من 12 سنة يعتبر بالغ",
+    },
+    {
+        hotelName: "Siam Kempinski Bangkok",
+        messageInfo_p1: "سيام كمبينسكي الشخص الي عمره 13 سنة وفوق لازم تأخذ له اكسترا بيد",
+    },
+    {
+        hotelName: "The Charm Resort Phuket",
+        messageInfo_p1: "ذا تشارم الشخص الي عمره 12 سنة وفوق يعتبر بالغ",
+    },
+    {
+        hotelName: "Amari Phuket",
+        messageInfo_p1: "اماري بوكيت الشخص الي عمره 12 سنة وفوق يعتبر بالغ",
+    },
+    {
+        hotelName: "Sofitel Bangkok Sukhumvit",
+        messageInfo_p1: "سوفيتيل بانكوك الشخص الي عمره 12 سنة وفوق يعتبر بالغ",
+    },
+    {
+        hotelName: "Dusit Thani Laguna Phuket",
+        messageInfo_p1: "دوسيت ثاني الشخص الي عمره 12 سنة وفوق يعتبر بالغ",
+    },
+];
